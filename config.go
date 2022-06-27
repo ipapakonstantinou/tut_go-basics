08873a8191f04ae1ddfd304687fc6769b7e7b325
ioannispapakonstantinou <johnypapakonstantinou@gmail.com>
2022-06-27 16:36:31 +0200

    first commit

---
+++ config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+    auth_token	string `mapstructure:"AUTH_TOKEN"`
+}
+
+func loadConfig() (auth_token string){
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	auth_token = fmt.Sprint(viper.Get("auth_token"))
+
+	return auth_token
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ipapakonstantinou/tut_go-basics
+
+go 1.18
